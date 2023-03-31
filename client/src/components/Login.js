ffd832dda3d3b49003db93ffea102f19e98d5933
Deniorrr <kujonek95@gmail.com>
2023-03-31 20:40:53 +0200

    login fixed

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -2,64 +2,65 @@ import React, { useState } from "react";
 import { NavLink } from "react-router-dom";
 import logo from "../assets/logo.png";
 import Undraw_image from "../assets/undraw1.svg";
+import axios from "axios";
 import "./style/Register.scss";
 function Login() {
-	const [username, setUsername] = useState("");
-	const [password, setPassword] = useState("");
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
 
-	const submitForm = () => {
-		axios
-			.post("http://localhost:3001/login", {
-				username: username,
-				password: password,
-			})
-			.then((response) => {
-				console.log(response);
-			});
-	};
-	return (
-		<div className="container" id="registerPanel">
-			<aside>
-				<div className="logo">
-					<img src={logo} alt="logo" />
-					<h1>Gym App</h1>
-				</div>
-				<figure>
-					<img src={Undraw_image} alt="Mobile phone" />
-				</figure>
-				<p className="caption">Soon on mobile!</p>
-			</aside>
-			<main>
-				<h2>Login</h2>
-				<p id="registerLink">
-					No account?<NavLink to="/profile"> Register</NavLink>
-				</p>
-				<div className="form">
-					<label htmlFor="username">
-						<p>Username</p>
-					</label>
-					<input
-						type="text"
-						id="username"
-						onChange={(e) => {
-							setUsername(e.target.value);
-						}}
-					/>
-
-					<label for="password">
-						<p>Password</p>
-					</label>
-					<input 
-						type="password" 
-						id="password" 
-						onChange={(e) => {
-							setPassword(e.target.value);
-						}}/>
-					<button>Login</button>
-				</div>
-			</main>
-		</div>
-	);
+  const submitForm = () => {
+    axios
+      .post("http://localhost:3001/login", {
+        username: username,
+        password: password,
+      })
+      .then((response) => {
+        console.log(response);
+      });
+  };
+  return (
+    <div className="container" id="registerPanel">
+      <aside>
+        <div className="logo">
+          <img src={logo} alt="logo" />
+          <h1>Gym App</h1>
+        </div>
+        <figure>
+          <img src={Undraw_image} alt="Mobile phone" />
+        </figure>
+        <p className="caption">Soon on mobile!</p>
+      </aside>
+      <main>
+        <h2>Login</h2>
+        <p id="registerLink">
+          No account?<NavLink to="/profile"> Register</NavLink>
+        </p>
+        <div className="form">
+          <label htmlFor="username">
+            <p>Username</p>
+          </label>
+          <input
+            type="text"
+            id="username"
+            onChange={(e) => {
+              setUsername(e.target.value);
+            }}
+          />
+          <label for="password">
+            <p>Password</p>
+          </label>
+          <input
+            type="password"
+            id="password"
+            onChange={(e) => {
+              setPassword(e.target.value);
+            }}
+          />
+          <button onClick={() => submitForm()}>Login</button>
+        </div>
+      </main>
+    </div>
+  );
 }
 
 export default Login;
--- client/src/components/Register.js
+++ client/src/components/Register.js
@@ -5,76 +5,76 @@ import Undraw_image from "../assets/undraw2.svg";
 import axios from "axios";
 import "./style/Register.scss";
 function Register() {
-	const [username, setUsername] = useState("");
-	const [email, setEmail] = useState("");
-	const [password, setPassword] = useState("");
+  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
-	const submitForm = () => {
-		axios
-			.post("http://localhost:3001/register", {
-				username: username,
-				email: email,
-				password: password,
-			})
-			.then((response) => {
-				console.log(response);
-			});
-	};
-	return (
-		<div className="container" id="registerPanel">
-			<aside>
-				<div className="logo">
-					<img src={logo} alt="logo" />
-					<h1>Gym App</h1>
-				</div>
-				<figure>
-					<img src={Undraw_image} alt="People exercising" />
-				</figure>
-				<p className="caption">Start documenting your progress today!</p>
-			</aside>
-			<main>
-				<h2>Register</h2>
-				<p id="loginLink">
-					Already registered? <NavLink to="/profile"> Log In</NavLink>
-				</p>
-				<div className="form">
-					<label htmlFor="username">
-						<p>Username</p>
-					</label>
-					<input
-						type="text"
-						id="username"
-						onChange={(e) => {
-							setUsername(e.target.value);
-						}}
-					/>
+  const submitForm = () => {
+    axios
+      .post("http://localhost:3001/register", {
+        username: username,
+        email: email,
+        password: password,
+      })
+      .then((response) => {
+        console.log(response);
+      });
+  };
+  return (
+    <div className="container" id="registerPanel">
+      <aside>
+        <div className="logo">
+          <img src={logo} alt="logo" />
+          <h1>Gym App</h1>
+        </div>
+        <figure>
+          <img src={Undraw_image} alt="People exercising" />
+        </figure>
+        <p className="caption">Start documenting your progress today!</p>
+      </aside>
+      <main>
+        <h2>Register</h2>
+        <p id="loginLink">
+          Already registered? <NavLink to="/profile"> Log In</NavLink>
+        </p>
+        <div className="form">
+          <label htmlFor="username">
+            <p>Username</p>
+          </label>
+          <input
+            type="text"
+            id="username"
+            onChange={(e) => {
+              setUsername(e.target.value);
+            }}
+          />
 
-					<label htmlFor="email">
-						<p>Email address</p>
-					</label>
-					<input
-						type="email"
-						id="email"
-						onChange={(e) => {
-							setEmail(e.target.value);
-						}}
-					/>
+          <label htmlFor="email">
+            <p>Email address</p>
+          </label>
+          <input
+            type="email"
+            id="email"
+            onChange={(e) => {
+              setEmail(e.target.value);
+            }}
+          />
 
-					<label htmlFor="password">
-						<p>Password</p>
-					</label>
-					<input
-						type="password"
-						id="password"
-						onChange={(e) => {
-							setPassword(e.target.value);
-						}}
-					/>
-					<button onClick={() => submitForm()}>Register</button>
-				</div>
-			</main>
-		</div>
-	);
+          <label htmlFor="password">
+            <p>Password</p>
+          </label>
+          <input
+            type="password"
+            id="password"
+            onChange={(e) => {
+              setPassword(e.target.value);
+            }}
+          />
+          <button onClick={() => submitForm()}>Register</button>
+        </div>
+      </main>
+    </div>
+  );
 }
 
 export default Register;
